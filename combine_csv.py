d209dc52a3e58f9769cca4baa8076e3d944fa344
ArnoldKuo <48876495+ArnoldKuo@users.noreply.github.com>
2019-05-13 08:05:33 +0800

    Add files via upload

--- combine_csv.py
+++ combine_csv.py
@@ -1,10 +1,22 @@
 import glob
 import pandas as pd
 
-extension = 'csv'
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+#extension = 'csv'
+#filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+datapath  = 'data/'
+year      = '2018'
+month     = '01'
+currency1 = 'USD'
+currency2 = 'NTD'
+
+months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+filenames = []
+frames = []
+for month in months:
+	filenames.append(datapath+currency1+currency2+"_"+year+month+'.csv')
 
 #combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+combined_csv = pd.concat([pd.read_csv(f) for f in filenames ])
 #export to csv
-combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+combined_csv.to_csv( datapath+year+"_forex_combined.csv", index=False, encoding='utf-8-sig')
